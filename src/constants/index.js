7e9916e8c20edb46750a6e91e6e123ce81a1a79e
Divya Chahal <divyachahal@outlook.com>
2023-03-17 01:33:33 +0000

    changes

--- src/constants/index.js
+++ src/constants/index.js
@@ -122,10 +122,10 @@ const experiences = [
    iconBg: "#383E56",
    date: "March 2020 - April 2021",
    points: [
-     "Developing and maintaining web applications using React.js and other related technologies.",
-     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-     "Implementing responsive design and ensuring cross-browser compatibility.",
-     "Participating in code reviews and providing constructive feedback to other developers.",
+    //  "Developing and maintaining web applications using React.js and other related technologies.",
+    //  "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+    //  "Implementing responsive design and ensuring cross-browser compatibility.",
+    //  "Participating in code reviews and providing constructive feedback to other developers.",
    ],
  },
  {
